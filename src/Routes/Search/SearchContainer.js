c9a6eda6f56a362b76d8abf71463b0051674d31a
hjcdb <hjcwinner@naver.com>
2020-09-30 17:49:10 +0900

    Search container #1

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,34 +1,54 @@
-import React, {useState} from 'react';
-import { movieApi, tvApi } from '../../api'
+// import React, {useState} from 'react';
+// import { movieApi, tvApi } from '../../api'
+// import SearchPresenter from './SearchPresenter'
+
+
+// const SearchContainer = () => {
+//     const [keyword, setKeyword ] = useState("")
+//     const [ results, setResults ] = useState({
+//         movies : [],
+//         shows : [],
+//         movieError : null,
+//         showError : null,
+//         loading : false
+//     })
+
+//     const onChange = (e) => {
+//        setKeyword(e.target.value)
+//     }
+
+//     const onSubmit = async () => {
+        
+//     }
+
+
+//     return (
+//        <SearchPresenter
+//             {...results} 
+//             onChange={onChange}
+//             onSubmit={onSubmit}
+//             keyword={keyword}
+//        />
+//     );
+// };
+
+// export default SearchContainer;
+
+import React, { useState } from 'react';
 import SearchPresenter from './SearchPresenter'
 
-
 const SearchContainer = () => {
-    const [keyword, setKeyword ] = useState("")
-    const [ results, setResults ] = useState({
-        movies : [],
-        shows : [],
-        movieError : null,
-        showError : null,
-        loading : false
+    const [ keyword, setKeyword] = useState("")
+    const [ results, setResults] = useState({
+        movieResults : [],
+        tvResults : [],
+        loading : false,
+        error : null
     })
 
-    const onChange = (text) => {
-       setKeyword(text)
-    }
-
-    const onSubmit = async () => {
-
-    }
-
-
     return (
-       <SearchPresenter
-            {...results} 
-            onChange={onChange}
-            onSubmit={onSubmit}
-            keyword={keyword}
-       />
+
+        <SearchPresenter />
     );
 };
 
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,45 +1,62 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components'
+
+// const Container = styled.div`
+//     padding : 0px 20px;
+// `;
+
+// const Form = styled.form`
+//     margin-bottom : 60px;
+//     width : 100%;
+// `;
+
+// const Input = styled.input`
+//     all: unset;
+//     font-size: 30px;
+//     width: 100%;
+// `;
+
+
+// const SearchPresenter = ({movies, shows, keyword, onChange, onSubmit, loading, error}) => {
+//     return (
+//         <Container>
+//             <Form onSubmit={onSubmit}>
+//                 <Input 
+//                     placeholder="Search Movies or TV shows..."
+//                     value={keyword}
+//                     onChange={onChange}
+//                 />   
+//             </Form>
+//         </Container>
+//     );
+// };
+
+// SearchPresenter.propTypes = {
+//     movies : PropTypes.array,
+//     shows : PropTypes.array,
+//     keyword : PropTypes.string,
+//     onChange : PropTypes.func.isRequired,
+//     onSubmit : PropTypes.func.isRequired,
+//     loading : PropTypes.bool.isRequired,
+//     error : PropTypes.string
+// };
+
+// export default SearchPresenter;
+
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components'
 
-const Container = styled.div`
-    padding : 0px 20px;
-`;
-
-const Form = styled.form`
-    margin-bottom : 60px;
-    width : 100%;
-`;
-
-const Input = styled.input`
-    all: unset;
-    font-size: 30px;
-    width: 100%;
-`;
-
-
-const SearchPresenter = ({movies, shows, keyword, onChange, onSubmit, loading, error}) => {
+const SearchPresenter = props => {
     return (
-        <Container>
-            <Form onSubmit={onSubmit}>
-                <Input 
-                    placeholder="Search Movies or TV shows..."
-                    value={keyword}
-                    onChange={onChange}
-                />   
-            </Form>
-        </Container>
+        <div>
+            <h1>hahaha</h1>  
+        </div>
     );
 };
 
 SearchPresenter.propTypes = {
-    movies : PropTypes.array,
-    shows : PropTypes.array,
-    keyword : PropTypes.string,
-    onChange : PropTypes.func.isRequired,
-    onSubmit : PropTypes.func.isRequired,
-    loading : PropTypes.bool.isRequired,
-    error : PropTypes.string
+    
 };
 
 export default SearchPresenter;
